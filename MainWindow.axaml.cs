0c1b2ea2f56e37f040a1cd1d85f7d7bd77390db8
THolden-dev <taimaholden@gmail.com>
2026-06-15 19:07:32 +0100

    Added gui textboxes for user input for abstract length and search term

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -14,6 +14,22 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        // Set initial size after window is opened
+        Opened += OnOpened;
+
+        // Update whenever the window size changes
+        SizeChanged += OnSizeChanged;
+    }
+
+    private void OnOpened(object? sender, System.EventArgs e)
+    {
+        UpdateResultsHeight();
+    }
+
+    private void OnSizeChanged(object? sender, SizeChangedEventArgs e)
+    {
+        UpdateResultsHeight();
     }
 
     public static List<int> SearchWordInDoc(string TextToSearch, string TermToSearch)
@@ -75,15 +91,51 @@ public partial class MainWindow : Window
         }
     }
 
+    public static bool VerifyInteger(string StringToVerify)
+    {
+        foreach (char Character in StringToVerify)
+        {
+            if (Character > '9' || Character < '0')
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void UpdateResultsHeight()
+    {
+        if (ResultsScrollSection != null)
+        {
+            // Limit results area to 50% of the window height
+            ResultsScrollSection.MaxHeight = Bounds.Height * 0.5;
+
+        }
+    }
     public void button_test(object sender, RoutedEventArgs e)
     {
+        string TermToSearch = SearchTermTextBox.Text;
+        Console.WriteLine("Clicked");
+        //Default value is 100
+        int NumberOfCharsToDisplay = 100;
+
+        if (VerifyInteger(CharacterLengthTextBox.Text))
+        {
+            Console.WriteLine("Verification successful");
+            NumberOfCharsToDisplay = Int32.Parse(CharacterLengthTextBox.Text);
+        }
+
         DirectoryInfo Directories = new DirectoryInfo("SearchArea");
         FileInfo [] Files = Directories.GetFiles();
+        Dictionary<string, int> MatchCounts = new Dictionary<string, int>();
+
         foreach(FileInfo FileInDir in Files)
         {           
             WordDocument DocToSearch = new WordDocument("SearchArea/" + FileInDir.Name);
             string TextToSearch = DocToSearch.ExtractText();
-            List <int> Indices = SearchWordInDoc(TextToSearch, "However");
+            List <int> Indices = SearchWordInDoc(TextToSearch, TermToSearch);
+            MatchCounts.Add(FileInDir.Name, Indices.Count);
             OutputSearchResults(Indices, TextToSearch);
         }
         
